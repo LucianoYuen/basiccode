de3680e2dd8629a6e37cc371c9dca9f122275479
LucianoYuen <74725605+LucianoYuen@users.noreply.github.com>
2021-02-12 10:55:22 +0800

    Create COUNTOF(__BUFFER__).c

---
+++ COUNTOF(__BUFFER__).c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdint.h> //uint8_t
+ 
+#define byte uint8_t
+ 
+#define COUNTOF(__BUFFER__)   (sizeof(__BUFFER__) / sizeof(*(__BUFFER__)))
+ 
+#define TXBUFFERSIZE	(COUNTOF(aTxBuffer) - 1)
+ 
+uint8_t aTxBuffer[] = " ****I2C_TwoBoards communication based on Polling****  ****I2C_TwoBoards communication based on Polling****  ****I2C_TwoBoards communication based on Polling**** ";
+
+int main(void) {
+ 
+	printf("EEPROM_Buffer[1]的值：%d\n", TXBUFFERSIZE);
+ 
+	return 0;
+}
+
+
+
+
+
+
+
+
+
