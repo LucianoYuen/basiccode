d04b4754ef9fa20c72703ce055ed777a7f1e59f5
LucianoYuen <74725605+LucianoYuen@users.noreply.github.com>
2021-05-09 22:17:33 +0800

    Update BT816_text_test1.c

--- BT816_text_test1.c
+++ BT816_text_test1.c
@@ -11,30 +11,32 @@ struct Notepad_buffer {
 	char notepad[MAX_LINES];
 }Buffer;
 
-int Read_Keypad = 1;
+int Read_Keypad = 0;
 int Disp_pos = 0;
 int line = 0;
-
+int key = 0;
 
 int main(void) {
 	
 do{	
+	key = Read_Keypad;
+	if(key != 255)
+	{
 	Buffer.notepad[line] = Read_Keypad;
 	printf("Buffer.notepad[line]：%d\n", Buffer.notepad[line]);
 	Read_Keypad++;
 	line++;
 	Disp_pos++;
-
+	}
+	
 	if(Disp_pos == 8)
 	{
+		if((Buffer.notepad[0] == 0) && (Buffer.notepad[1] == 1) && (Buffer.notepad[2] == 2))
+		{
+		printf("test OK!\n");
+		}
 	Disp_pos = 0;
 	}
 
 }while(Disp_pos);
-
-	if((Buffer.notepad[0] == 1) && (Buffer.notepad[1] == 1) && (Buffer.notepad[2] == 3))
-	{
-	printf("test OK!\n");
-	}
-
 }
