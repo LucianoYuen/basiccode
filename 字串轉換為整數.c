a8c86b15d498764cf3667e0d2633f886fd91e36f
LucianoYuen <74725605+LucianoYuen@users.noreply.github.com>
2021-05-31 23:40:32 +0800

    Update 字串轉換為整數.c

--- 字串轉換為整數.c
+++ 字串轉換為整數.c
@@ -4,17 +4,35 @@
 #include <stdlib.h>
 #include <string.h>
 
-char d_1[]  = "10000";
+char d_1[6]  = "12346";
+
 char d_2[]  = "10100";
+
 int value;
 
 int main(void) {
 
-    d_1;
-    printf("String value = %s, Int value = %d\n", d_1, value);
-    strcpy(d_1,d_2);
-    value = atoi(d_1);
-    printf("String value = %s, Int value = %d\n", d_1, value);
+    printf("total_data_count的值：%d\n", d_1[6]);
+    
+    printf("total_data_count的值：%d\n", d_1[5]);    
+    
+	printf("total_data_count的值：%d\n", d_1[4]);  
+    
+    printf("total_data_count的值：%d\n", d_1[3]); 
+    
+    printf("total_data_count的值：%d\n", d_1[2]);  
+    
+    printf("total_data_count的值：%d\n", d_1[1]);
+    
+    printf("total_data_count的值：%d\n", d_1[0]);
+    
+    printf("String value = %s, Int value = %d\n", d_2, value);
+    
+    strcpy(d_2,d_1);
+    
+    value = atoi(d_2);
+    
+    printf("String value = %s, Int value = %d\n", d_2, value);
     
     printf("total_data_count的值：%d\n", value);
     
